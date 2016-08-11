b8b2245b72f43be63a3cafb1f027d630ba4b4db4
crccheck <c@crccheck.com>
2016-08-11 18:40:47 -0500

    Add some more venues

---
+++ src/venues/athenian.js
@@ -0,0 +1,5 @@
+export const data = {
+  name: 'Athenian',
+  url: 'http://athenianbargrill.com',
+  coordinates: [30.2685019, -97.7456919],
+};
--- src/venues/mythaimom.js
+++ src/venues/mythaimom.js
@@ -1,4 +1,5 @@
 export const data = {
   name: 'My Thai Mom',
+  url: 'https://www.facebook.com/MyThaiMom',
   coordinates: [30.2720917, -97.7437975],
 };
