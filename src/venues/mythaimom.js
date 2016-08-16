670bd70440a1072945ee5c1b106a87b65e82fa9e
crccheck <c@crccheck.com>
2016-08-16 16:55:00 -0500

    add specials from My Thai Mom

--- src/venues/mythaimom.js
+++ src/venues/mythaimom.js
@@ -1,5 +1,22 @@
+import request from 'request-promise';
+
 export const data = {
   name: 'My Thai Mom',
+  // fb id: 200414600091489
   url: 'https://www.facebook.com/MyThaiMom',
   coordinates: [30.2720917, -97.7437975],
 };
+
+export async function scrape() {
+  const body = await request.get({
+    url: data.url,
+    qs: { access_token: process.env.FACEBOOK_APP_TOKEN },
+    json: true,
+  });
+  const now = new Date();
+  const recentPosts = body.data.filter((x) => (now - new Date(x.created_time)) < 86400 * 1000);
+  const recentMenuPosts = recentPosts.filter((x) => x.message.search(/menu/i) !== -1);
+  const menu = recentMenuPosts.length ? recentMenuPosts[0].message : undefined;
+
+  return recentMenuPosts.length && Object.assign({ menu }, data);
+}
---
+++ test/venues/mythaimom-test.js
@@ -0,0 +1,37 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import sinonAsPromised from 'sinon-as-promised'; // eslint-disable-line no-unused-vars
+import request from 'request-promise';
+import { scrape } from '../../src/venues/mythaimom';
+
+const FB_JSON = require('../fixtures/mythaimom.json');
+
+
+describe('mythaimom', () => {
+  describe('scrape with a menu', () => {
+    before(() => {
+      sinon.stub(request, 'get').returns(FB_JSON);
+    });
+    after(() => {
+      request.get.restore();
+    });
+    it('extracts the special of the day from the json', () =>
+      scrape().then((venueData) => {
+        expect(venueData.menu).to.include('stir fry');
+      })
+    );
+  });
+  describe('scrape without a menu', () => {
+    before(() => {
+      sinon.stub(request, 'get').returns({ data:[] });
+    });
+    after(() => {
+      request.get.restore();
+    });
+    it('returns nothing when there is no menu', () =>
+      scrape().then((venueData) => {
+        expect(venueData.menu).to.be.undefined;
+      })
+    );
+  });
+});
