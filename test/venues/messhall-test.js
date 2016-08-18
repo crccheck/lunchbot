c6d0c908151f50be4cd9c52b01919fc0f10f2ec0
crccheck <c@crccheck.com>
2016-08-18 14:00:36 -0500

    Add mess hall schedules

--- test/venues/messhall-test.js
+++ test/venues/messhall-test.js
@@ -4,30 +4,24 @@ import sinon from 'sinon';
 import sinonAsPromised from 'sinon-as-promised'; // eslint-disable-line no-unused-vars
 import path from 'path';
 import request from 'request-promise';
-import { data, scrape } from '../../src/venues/slake';
+import { scrape } from '../../src/venues/messhall';
 
-describe('slake', () => {
-  describe('data', () => {
-    it('has a name', () => {
-      expect(data.name).to.be.ok;
-    });
-    it('has a url', () => {
-      expect(data.url).to.be.ok;
-    });
-  });
-
-  describe('scrape', () => {
+describe('messhall', () => {
+  describe('scrape with a menu', () => {
+    let clock;
     before(() => {
-      const fixturePath = path.join(__dirname, '../fixtures/slake.html');
+      const fixturePath = path.join(__dirname, '../fixtures/messhall.html');
       const dummyHTML = readFileSync(fixturePath, { encoding: 'utf8' });
       sinon.stub(request, 'get').returns(dummyHTML);
+      clock = sinon.useFakeTimers(new Date('2016-08-18T11:00:00-05:00').getTime()); // Thursday
     });
     after(() => {
+      clock.restore();
       request.get.restore();
     });
     it('extracts the special of the day from the HTML', () =>
       scrape().then((venueData) => {
-        expect(venueData.menu.text()).to.include('Quinoa');
+        expect(venueData.menu.text()).to.include('Pork Chops');
       })
     );
   });
